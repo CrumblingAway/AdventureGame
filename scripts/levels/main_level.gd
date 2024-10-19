3204396c69a95055ab1a7f3c32e681c803f75342
CrumblingAway <antonkid@gmail.com>
2024-10-19 22:16:35 +0300

    Levels: Delete temp levels.

--- scripts/levels/main_level.gd
+++ scripts/levels/main_level.gd
@@ -4,16 +4,17 @@ var _main_level : Level
 var _player : Player
 
 func _ready():
-	_main_level = preload("res://levels/test_level.tscn").instantiate()
-	_player = preload("res://objects/Player.tscn").instantiate()
-	add_child(_player)
-	
-	Global.set_player(_player)
-	InputManager.set_player(_player)
-	
-	var levels : Array[Level] = [_main_level]
-	LevelManager.setup_with_levels(levels)
-	LevelManager.run()
+#	_main_level = preload("res://levels/test_level.tscn").instantiate()
+#	_player = preload("res://objects/Player.tscn").instantiate()
+#	add_child(_player)
+#
+#	Global.set_player(_player)
+#	InputManager.set_player(_player)
+#
+#	var levels : Array[Level] = [_main_level]
+#	LevelManager.setup_with_levels(levels)
+#	LevelManager.run()
+	pass
 
 func _process(delta: float) -> void:
 	pass
