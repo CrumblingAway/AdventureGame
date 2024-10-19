d5cfb4a95917d23df8988c96bb6e31857b45a60c
CrumblingAway <antonkid@gmail.com>
2024-10-19 22:51:17 +0300

    TestLevel: Add main level and hosue floors.

--- scripts/levels/main_level.gd
+++ scripts/levels/main_level.gd
@@ -4,17 +4,16 @@ var _main_level : Level
 var _player : Player
 
 func _ready():
-#	_main_level = preload("res://levels/test_level.tscn").instantiate()
-#	_player = preload("res://objects/Player.tscn").instantiate()
-#	add_child(_player)
-#
-#	Global.set_player(_player)
-#	InputManager.set_player(_player)
-#
-#	var levels : Array[Level] = [_main_level]
-#	LevelManager.setup_with_levels(levels)
-#	LevelManager.run()
-	pass
+	_main_level = preload("res://levels/test_level.tscn").instantiate()
+	_player = preload("res://objects/Player.tscn").instantiate()
+	add_child(_player)
+
+	Global.set_player(_player)
+	InputManager.set_player(_player)
+
+	var levels : Array[Level] = [_main_level]
+	LevelManager.setup_with_levels(levels)
+	LevelManager.run()
 
 func _process(delta: float) -> void:
 	pass
