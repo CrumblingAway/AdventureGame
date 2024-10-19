b569e56ca6677d524c732782bcdb3d9ea33ff4e3
CrumblingAway <antonkid@gmail.com>
2024-10-19 16:01:54 +0300

    Sublevels: Add test sublevel 2.

---
+++ scripts/levels/test_sublevel_2.gd
@@ -0,0 +1,21 @@
+extends Level
+
+func _ready():
+	super._ready()
+
+func _process(delta: float) -> void:
+	if _player:
+		_last_player_location = _player.global_position
+
+func enter() -> void:
+	_player = Global.get_player()
+	if _player.get_parent():
+		_player.reparent(self, false)
+	else:
+		add_child(_player)
+	
+	_player.global_position = _last_player_location
+	_player.attach_to_grid(_grid)
+
+func exit() -> void:
+	_player = null
