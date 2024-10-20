9639a0c1c9fa0fe93d04dc835566583179ab379d
CrumblingAway <antonkid@gmail.com>
2024-10-20 23:07:15 +0300

    Health: Add basic health component.

---
+++ scripts/health_component.gd
@@ -0,0 +1,43 @@
+class_name Health extends Node
+
+########## Signals. ##########
+
+signal health_reached_zero
+signal health_reached_max
+
+########## Fields. ##########
+
+@export var max_health_points : int
+@export var health_points : int:
+	get:
+		return _health_points
+	set(value):
+		_health_points = value
+		if _health_points == 0:
+			health_reached_zero.emit()
+		elif _health_points == _max_health_points:
+			health_reached_max.emit()
+
+########## Health methods. ##########
+
+func add_health(amount: int) -> void:
+	health_points = min(max_health_points, health_points + amount)
+
+func subtract_health(amount: int) -> void:
+	health_points = max(0, health_points - amount)
+
+func increase_max_health(amount: int) -> void:
+	max_health_points += amount
+
+func decrease_max_health(amount: int) -> void:
+	max_health_points = max(0, max_health_points - amount)
+	health_points = min(health_points, max_health_points)
+
+########## Node methods. ##########
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
