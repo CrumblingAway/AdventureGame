6d92db9c28c1af60b62eb61327003fc12bb8cd25
CrumblingAway <antonkid@gmail.com>
2024-10-21 15:55:03 +0300

    InputManager: Add enum states.

--- scripts/autoload/input_manager.gd
+++ scripts/autoload/input_manager.gd
@@ -1,8 +1,21 @@
 extends Node
 
+
+enum Mode
+{
+	MENU,
+	COMBAT_SEARCH,
+	COMBAT,
+	MOVEMENT,
+	NUM_OF_MODES
+}
+
 var _player : Player
 
-var _is_game_paused : bool = false
+var _mode : Mode = Mode.MOVEMENT
+
+#var _is_game_paused : bool = false
+#var _is_combat_search_mode : bool = false
 
 ########## Node methods. ##########
 
@@ -10,43 +23,62 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	if not _player:
-		return
-	
-	if not _is_game_paused:
-		var current_level : Level = LevelManager.get_current_level()
-		if current_level:
-			var direction : Vector2i = Vector2i.ZERO
-			if Input.is_action_just_pressed("move_up"):
-				direction = Vector2i.UP
-			elif Input.is_action_just_pressed("move_down"):
-				direction = Vector2i.DOWN
-			elif Input.is_action_just_pressed("move_left"):
-				direction = Vector2i.LEFT
-			elif Input.is_action_just_pressed("move_right"):
-				direction = Vector2i.RIGHT
-			_player.move(direction)
-
-			if Input.is_action_just_pressed("action"):
-				current_level.activate_transition_at(_player.global_position)
-		elif LevelManager.get_current_combat_level():
+	match _mode:
+		Mode.MENU:
 			pass
+		Mode.COMBAT_SEARCH:
+			pass
+		Mode.COMBAT:
+			pass
+		Mode.MOVEMENT:
+			if not _player:
+				return
+				
+			var current_level : Level = LevelManager.get_current_level()
+			if current_level:
+				var direction : Vector2i = Vector2i.ZERO
+				if Input.is_action_just_pressed("move_up"):
+					direction = Vector2i.UP
+				elif Input.is_action_just_pressed("move_down"):
+					direction = Vector2i.DOWN
+				elif Input.is_action_just_pressed("move_left"):
+					direction = Vector2i.LEFT
+				elif Input.is_action_just_pressed("move_right"):
+					direction = Vector2i.RIGHT
+				_player.move(direction)
+
+				if Input.is_action_just_pressed("action"):
+					current_level.activate_transition_at(_player.global_position)
+		_:
+			print("Invalid input mode.")
 	
-#	if current_level and not _is_game_paused:
-#		if _player:
-#			var direction : Vector2i = Vector2i.ZERO
-#			if Input.is_action_just_pressed("move_up"):
-#				direction = Vector2i.UP
-#			elif Input.is_action_just_pressed("move_down"):
-#				direction = Vector2i.DOWN
-#			elif Input.is_action_just_pressed("move_left"):
-#				direction = Vector2i.LEFT
-#			elif Input.is_action_just_pressed("move_right"):
-#				direction = Vector2i.RIGHT
-#			_player.move(direction)
+#	if not _player:
+#		return
 #
-#			if Input.is_action_just_pressed("action"):
-#				current_level.activate_transition_at(_player.global_position)
+#	if not _is_game_paused:
+#		if Input.is_action_just_pressed("combat_mode"):
+#			_is_combat_search_mode = not _is_combat_search_mode
+#
+#		if not _is_combat_search_mode:
+#			var current_level : Level = LevelManager.get_current_level()
+#			if current_level:
+#				var direction : Vector2i = Vector2i.ZERO
+#				if Input.is_action_just_pressed("move_up"):
+#					direction = Vector2i.UP
+#				elif Input.is_action_just_pressed("move_down"):
+#					direction = Vector2i.DOWN
+#				elif Input.is_action_just_pressed("move_left"):
+#					direction = Vector2i.LEFT
+#				elif Input.is_action_just_pressed("move_right"):
+#					direction = Vector2i.RIGHT
+#				_player.move(direction)
+#
+#				if Input.is_action_just_pressed("action"):
+#					current_level.activate_transition_at(_player.global_position)
+#			elif LevelManager.get_current_combat_level():
+#				pass
+#		else:
+#			pass
 
 ########## InputManager methods. ##########
 
--- scripts/levels/main_level.gd
+++ scripts/levels/main_level.gd
@@ -5,27 +5,27 @@ var _combat_level : CombatLevel
 var _player : Player
 
 func _ready():
-#	_main_level = preload("res://levels/test_level.tscn").instantiate()
-#	_player = preload("res://objects/Player.tscn").instantiate()
-#	add_child(_player)
-#
-#	Global.set_player(_player)
-#	InputManager.set_player(_player)
-#
-#	var levels : Array[Level] = [_main_level]
-#	LevelManager.setup_with_levels(levels)
-#	LevelManager.run()
-	_combat_level = preload("res://levels/test_combat_level.tscn").instantiate()
+	_main_level = preload("res://levels/test_level.tscn").instantiate()
 	_player = preload("res://objects/Player.tscn").instantiate()
-	var enemies : Array[Enemy] = [
-		preload("res://objects/Enemy.tscn").instantiate() as Enemy,
-		preload("res://objects/Enemy.tscn").instantiate() as Enemy,
-		preload("res://objects/Enemy.tscn").instantiate() as Enemy
-	]
-	var enemy_distances : Array[int] = [5, 2, 7]
-	
-	add_child(_combat_level)
-	_combat_level.setup(_player, enemies, enemy_distances)
+	add_child(_player)
+
+	Global.set_player(_player)
+	InputManager.set_player(_player)
+
+	var levels : Array[Level] = [_main_level]
+	LevelManager.setup_with_levels(levels)
+	LevelManager.run()
+#	_combat_level = preload("res://levels/test_combat_level.tscn").instantiate()
+#	_player = preload("res://objects/Player.tscn").instantiate()
+#	var enemies : Array[Enemy] = [
+#		preload("res://objects/Enemy.tscn").instantiate() as Enemy,
+#		preload("res://objects/Enemy.tscn").instantiate() as Enemy,
+#		preload("res://objects/Enemy.tscn").instantiate() as Enemy
+#	]
+#	var enemy_distances : Array[int] = [5, 2, 7]
+#
+#	add_child(_combat_level)
+#	_combat_level.setup(_player, enemies, enemy_distances)
 
 func _process(delta: float) -> void:
 	pass
