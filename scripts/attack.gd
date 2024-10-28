03a702acc9e2f1544175ffa057991d93dad7f0e9
CrumblingAway <antonkid@gmail.com>
2024-10-28 19:28:43 +0200

    Attack: Remove attack resource.

--- scripts/attack.gd
+++
@@ -1,8 +0,0 @@
-class_name Attack extends Resource
-
-@export var damage_points : int
-
-func _init(
-	damage_points: int
-) -> void:
-	self.damage_points = damage_points
--- scripts/combat_manager.gd
+++ scripts/combat_manager.gd
@@ -2,11 +2,7 @@ class_name CombatManager extends Node
 
 ########## CombatManager methods. ##########
 
-func attack_target(
-	target_health: Health,
-	attack: Attack
-) -> void:
-	target_health.subtract_health(attack.damage_points)
+
 
 ########## Node methods. ##########
 
