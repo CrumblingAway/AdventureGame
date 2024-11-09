9305e41711578fe7e41cffecfc5b8643e7eee01d
CrumblingAway <antonkid@gmail.com>
2024-11-09 14:31:48 +0200

    CombatMenu: Add item menu setup.

--- scripts/levels/test_combat_level.gd
+++ scripts/levels/test_combat_level.gd
@@ -97,8 +97,6 @@ func _setup_player_menu() -> void:
 	
 	# Add buttons for items.
 	_setup_player_items_menu(player_inventory.get_node("Items"))
-	
-	pass
 
 func _setup_player_weapons_menu(weapons: Node) -> void:
 	# Connect buttons to top and bottom neighbors.
@@ -123,8 +121,28 @@ func _setup_player_weapons_menu(weapons: Node) -> void:
 		first_weapon_button.focus_neighbor_top = last_weapon_button.get_path()
 		last_weapon_button.focus_neighbor_bottom = first_weapon_button.get_path()
 
-func _setup_player_items_menu(weapons: Node) -> void:
-	pass
+func _setup_player_items_menu(items: Node) -> void:
+	# Connect buttons to top and bottom neighbors.
+	for item_idx in range(items.get_children().size() - 1, -1, -1):
+		var item : Weapon = items.get_children()[item_idx]
+		
+		var item_button_label : String = "%s (%d,%2.1f)" % [item._name, item._damage, item._accuracy]
+		var item_button : Button = Button.new()
+		item_button.text = item_button_label
+		
+		_player_combat_menu.get_node("ItemsMenu").add_child(item_button)
+		_player_combat_menu.get_node("ItemsMenu").move_child(item_button, 0)
+		
+		var next_button : Button = _player_combat_menu.get_node("ItemsMenu").get_child(1)
+		next_button.focus_neighbor_top = item_button.get_path()
+		item_button.focus_neighbor_bottom = next_button.get_path()
+	
+	# Connect top and bottom buttons.
+	if _player_combat_menu.get_node("ItemsMenu").get_child_count() > 1:
+		var first_item_button : Button = _player_combat_menu.get_node("ItemsMenu").get_child(0)
+		var last_item_button : Button = _player_combat_menu.get_node("ItemsMenu").get_child(_player_combat_menu.get_node("ItemsMenu").get_child_count() - 1)
+		first_item_button.focus_neighbor_top = last_item_button.get_path()
+		last_item_button.focus_neighbor_bottom = first_item_button.get_path()
 
 ########## Node2D methods. ##########
 
